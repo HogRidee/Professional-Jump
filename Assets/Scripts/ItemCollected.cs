d032f8ad1e13d45fd8b3955ccf94776385122489
HogRidee <iaraoz061102@gmail.com>
2024-01-08 18:14:38 -0500

    Added item collection

---
+++ Assets/Scripts/ItemCollected.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollected : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GetComponent<SpriteRenderer>().enabled = false;
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            Destroy(gameObject, 0.5f);
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,9 @@ public class PlayerMovement : MonoBehaviour
     public float runSpeed = 2.0f;
     public float jumpSpeed = 3.0f;
     private Rigidbody2D _rb2d;
+    public bool betterJump = false;
+    public float fallMultiplier = 0.5f;
+    public float lowJumpMultiplier = 1f;
     
     void Start()
     {
@@ -34,6 +37,19 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey("space") && CheckGround.IsGrounded)
         {
             _rb2d.velocity = new Vector2(_rb2d.velocity.x, jumpSpeed);
+        }
+
+        if (betterJump)
+        {
+            if (_rb2d.velocity.y < 0)
+            {
+                _rb2d.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier) * Time.deltaTime;
+            }
+
+            if (_rb2d.velocity.y > 0 && !Input.GetKey("space"))
+            {
+                _rb2d.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier) * Time.deltaTime;
+            }
         }
     }
 }
