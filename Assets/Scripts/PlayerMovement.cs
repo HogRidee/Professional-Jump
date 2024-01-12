3254cffd89ce7e3ef878c72f9e91335b2e1ecb3b
HogRidee <iaraoz061102@gmail.com>
2024-01-12 17:19:29 -0500

    Added jump and walk animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,9 +7,14 @@ public class PlayerMovement : MonoBehaviour
     public float runSpeed = 2.0f;
     public float jumpSpeed = 3.0f;
     private Rigidbody2D _rb2d;
+    
     public bool betterJump = false;
     public float fallMultiplier = 0.5f;
     public float lowJumpMultiplier = 1f;
+
+    public SpriteRenderer spriteRenderer;
+
+    public Animator animator;
     
     void Start()
     {
@@ -22,21 +27,37 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey("right"))
         {
             _rb2d.velocity = new Vector2(runSpeed, _rb2d.velocity.y);
+            spriteRenderer.flipX = false;
+            animator.SetBool("Walk", true);
         }
         
         else if (Input.GetKey("left"))
         {
             _rb2d.velocity = new Vector2(-runSpeed, _rb2d.velocity.y);
+            spriteRenderer.flipX = true;
+            animator.SetBool("Walk", true);
         }
         
         else
         {
             _rb2d.velocity = new Vector2(0, _rb2d.velocity.y);
+            animator.SetBool("Walk", false);
         }
         
         if (Input.GetKey("space") && CheckGround.IsGrounded)
         {
             _rb2d.velocity = new Vector2(_rb2d.velocity.x, jumpSpeed);
+        }
+
+        if (CheckGround.IsGrounded == false)
+        {
+            animator.SetBool("Jump", true);
+            animator.SetBool("Walk", false);
+        }
+        
+        if (CheckGround.IsGrounded == true)
+        {
+            animator.SetBool("Jump", false);
         }
 
         if (betterJump)
